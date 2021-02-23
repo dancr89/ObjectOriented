2cc48053c60e50ad0d76ab4b4c513415416c3cf1
Craciun Dan <craciunmdan@yahoo.com>
2021-02-23 15:04:08 +0200

    New Add

---
+++ main.py
@@ -0,0 +1,16 @@
+class Pizza:
+    price = 15
+
+    def __init__(self, name, ingredinete):
+        # print('pizza.__init__', name, ingredinete)
+        self.name = name
+        self.ingredinete = ingredinete
+
+if __name__ == '__main__':
+    print(Pizza.price)
+
+    my_pizza = Pizza('margerita', ['a', 'b', 'c'])
+    print(my_pizza)
+    print(my_pizza.name)
+    print(my_pizza.ingredinete)
+    print(my_pizza.price)
