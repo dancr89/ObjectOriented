828f138669da83b7564b3c20e010b044a56fefd3
Craciun Dan <craciunmdan@yahoo.com>
2021-02-25 15:28:48 +0200

    exceptii

---
+++ exceptii.py
@@ -0,0 +1,39 @@
+# user_input = input('> ')
+#
+# try:
+#     user_input = float(user_input)
+#     #print(name)
+#
+#     if user_input % 2 == 0:
+#         print(f'{user_input} is even')
+#     else:
+#         print(f'{user_input} is odd')
+#
+# # except (ValueError, NameError) as e:
+# #     print(f'{user_input} is not a number or NameError')
+# #     print(e)
+#
+# except Exception as e:
+#     print(type(e), e)
+#
+# print('end of application')
+
+
+# def my_function(even_number):
+#     if even_number % 2 != 0:
+#         raise ValueError(f'{even_number} is not an even number')
+#
+#     return True
+#
+# print(my_function(3))
+# # try:
+# #     print(my_function(3))
+# # except ValueError:
+# #     print('odd number used as parameter')
+#
+# print('end of the app')
+
+# print(dir(range))
+
+if __name__ == '__main__':
+        print('run code...')
